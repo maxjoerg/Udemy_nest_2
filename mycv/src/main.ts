8b87dcdb95b0f71cd2830169c5987869498c5e9a
maxjoerg <max.schleiwies@googlemail.com>
2022-08-04 18:27:48 +0200

    user service added, create user implemented

--- mycv/src/main.ts
+++ mycv/src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  (app as any).set('etag', false);
+  app.use((req, res, next) => {
+    res.removeHeader('x-powered-by');
+    res.removeHeader('date');
+    next();
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
