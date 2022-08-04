54228eeb503fb68bee8f2df0e4ddb682e3326d51
maxjoerg <max.schleiwies@googlemail.com>
2022-08-04 10:52:17 +0200

    service added

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 
